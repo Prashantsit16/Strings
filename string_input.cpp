105f4af807b1998c04cf8783059f3e9b93367239
Prashantsit16 <prashant.bajpai.btech2023@sitpune.edu.in>
2024-09-05 12:01:43 +0530

    string

---
+++ string_input.cpp
@@ -0,0 +1,13 @@
+//prashant
+//23070123102
+// taking input from the user 
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a[50];//declaring the string array 
+    cout<<"Enter a string: ";
+    cin>>a;
+    cout<<"Your inputed string was " <<a;
+    return 0;
+}
