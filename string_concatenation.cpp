a330452d03b988a71af7aa0b90dbde1c7b6b1d23
Prashantsit16 <prashant.bajpai.btech2023@sitpune.edu.in>
2024-09-05 12:02:15 +0530

    string

---
+++ string_concatenation.cpp
@@ -0,0 +1,17 @@
+//prashant
+//23070123102
+// taking input from the user 
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str1, str2, fstr;
+    cout<<"Enter a string: ";
+    cin>>str1;
+    cout<<"Enter 2nd string: ";
+    cin>>str2;
+    fstr=str1+" "+str2;//concatenation of strings 
+    cout<<fstr;
+    return 0;
+
+}
